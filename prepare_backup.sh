810a91170b435c20623dace0ba78b4e4a2b7da1b
Jayadeep KM <kmjayadeep@gmail.com>
2021-01-31 15:38:18 +0530

    new scripts

--- prepare_backup.sh
+++ prepare_backup.sh
@@ -2,11 +2,17 @@
 echo "Preparing backup"
 echo "copying dotfiles"
 cp ~/.zshrc ~/workspace/dotfiles/zsh/zshrc
+cp ~/.bash_aliases ~/workspace/dotfiles/zsh/bash_aliases
 cp ~/.Xresources ~/workspace/dotfiles/Xresources/Xresources
 cp ~/.vimrc ~/workspace/dotfiles/vim/vimrc
-cp ~/.config/nvim/init.vim ~/workspace/dotfiles/nvim/init.vim
-cp ~/.config/i3/config ~/workspace/dotfiles/i3/config
+cp -r ~/.config/nvim/ ~/workspace/dotfiles/
+cp ~/.config/i3/* ~/workspace/dotfiles/i3/ -r
 cp ~/.config/picom/picom.conf ~/workspace/dotfiles/picom/picom.conf
+cp ~/.config/kitty/kitty.conf ~/workspace/dotfiles/kitty/kitty.conf
+cp ~/.config/polybar/ ~/workspace/dotfiles/polybar -r
+
+echo "copying zsh history"
+cp ~/.zsh_history ~/workspace/backups/zsh/zsh_history
 
 echo "copying ssh"
 cp ~/.ssh/config ~/workspace/backups/ssh/
@@ -14,4 +20,6 @@ cp ~/.ssh/authorized_keys ~/workspace/backups/ssh/
 cp ~/.ssh/known_hosts ~/workspace/backups/ssh/
 
 echo "copying kubernetes config"
-cp -r ~/.kube ~/workspace/backups/kube
+cp ~/.kube/config ~/workspace/backups/kube/config
+cp ~/.kube/kubectx ~/workspace/backups/kube/kubectx
+cp ~/.kube/kubens ~/workspace/backups/kube/kubens -r
